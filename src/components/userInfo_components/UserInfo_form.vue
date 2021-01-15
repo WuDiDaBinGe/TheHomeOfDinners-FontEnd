d17d899843fc2deb1f0558010edd40cf14f88c00
1151680016@qq.com <1151680016@qq.com>
2021-01-15 15:39:08 +0800

    评分排序以及条件筛选餐馆功能优化

--- src/components/userInfo_components/UserInfo_form.vue
+++ src/components/userInfo_components/UserInfo_form.vue
@@ -7,13 +7,12 @@
             <input name="id" id="id" value="User_data.4" style="display: none">
             <input name="cho" id="cho" value="0" style="display: none">
 
-
             <div class="form-group">
                 <label>编辑头像</label>
                 <div class="fileupload"><input type="file"  accept="image/*"></div>
             </div>
             <div class="form-group">
-                <label>编辑手机号</label>
+                <label>修改手机号</label>
                 <input class="form-control" type="text"  :value="user.name">
             </div>
             <div class="form-group">
@@ -55,16 +54,9 @@ export default {
             user:{
                 name:"yuxiubin",
                 pic_src:"https://bkimg.cdn.bcebos.com/pic/5243fbf2b21193134c87adbe67380cd790238dfc?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2U4MA==,g_7,xp_5,yp_5",
-                profile:"",
-                email:"1151680016@qq.com",
-                city:"湖南长沙",
-                country:"",
                 password:"",
                 new_password:"",
                 comfir_password:"",
-                reviews_num:"",
-                reads_num:"",
-                userfule_num:"",
             }
 
         }
