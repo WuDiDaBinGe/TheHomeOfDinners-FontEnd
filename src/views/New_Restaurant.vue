5a0085f0e0fdd3ad1e125c4c6244cc5314857fa2
yanwangdedongxi <971469461@qq.com>
2021-01-17 15:54:32 +0800

    更改传输的数据，增加地区选项

--- src/views/New_Restaurant.vue
+++ src/views/New_Restaurant.vue
@@ -22,9 +22,9 @@
 						</div>
              <div class="form-group" >
              <label>餐馆类型：</label><span class="text-danger">*</span>
-               <div class="col-lg-3">
+               <div class="col-lg-4">
 									<select class="wide nice-select" name="find2" v-model="new_restaurant.res_tag">
-                    <option value="请选择">请选择</option>
+                    <option value="请选择">请选择餐馆类型</option>
 										<option v-for="item in tags_list" v-bind:value="item">{{item}}</option>
                   </select>
                </div>
@@ -32,7 +32,14 @@
             <br></br>
             <div class="form-group">
 							<label>地址：</label><span class="text-danger">*</span>
-							<input class="form-control" type="text"  placeholder="餐馆的地点" v-model="new_restaurant.res_address">
+              <div class="col-lg-4">
+
+									<select class="wide nice-select" name="find2" v-model="new_restaurant.res_region_tag">
+                    <option value="请选择">请选择地区</option>
+										<option v-for="item in regions_list" v-bind:value="item">{{item}}</option>
+                  </select>
+               </div>
+							<input class="form-control" type="text"  placeholder="餐馆的具体地点" v-model="new_restaurant.res_address">
 						</div>
             <div class="form-group">
 							<label>加一点餐馆和菜品的图片(可选)</label>
@@ -76,9 +83,11 @@
         name: "New_Restaurant",
         data(){
           return{
-            tags_list:["All Categories","Lebanese","Cafe","Bar","Pizza","Seafood","Roast","Spaghetti","Dessert"],
+            tags_list:["1","2","3"],
+            regions_list:[],
             new_restaurant:{
               res_name:"",
+              res_region_tag:"请选择",
               res_address:"",
               res_tag:"请选择",
               picture:null,
@@ -107,6 +116,12 @@
               this.$message.error("请选择餐馆类型！");
               return;
             }
+             if(restaurantInfo.res_region_tag==="请选择"){
+              this.$message.error("请选择地区！");
+              return;
+            }
+            let res_tag_id= this.tags_list.findIndex(item => item===restaurantInfo.res_tag);
+             let res_region_id = this.regions_list.findIndex(item => item===restaurantInfo.res_region_tag);
             this.new_restaurant.owner=JSON.parse(getLocalStore("userLogin"))['user_id'];
             let param=new FormData();
             param.append("res_name",this.new_restaurant.res_name);           //向对象中添加数据
@@ -115,7 +130,8 @@
             param.append("business_time",this.new_restaurant.business_time);
             param.append("mobile",this.new_restaurant.mobile);
             param.append("owner",this.new_restaurant.owner);
-            param.append("res_tag",this.new_restaurant.res_tag);
+            param.append("tag1",res_tag_id);
+            param.append("tag2",res_region_id);
             this.$httpM.post(this.$api.Restaurant.create,param,false)
             .then(function (response) {
               console.log("response:",response);
@@ -130,6 +146,7 @@
             .then(function (response) {
 
                 tmpThis.tags_list=response.data['种类'];
+                tmpThis.regions_list=response.data['地区'];
             })
             .catch(function (err) {
                 console.log(err);
